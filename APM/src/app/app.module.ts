0b5a36660bb03c765c10a5227142764c6b5a7863
jtwalsh101 <jtwalsh101@users.noreply.github.com>
2020-04-12 17:36:20 -0500

    Comnpleted Chapter 5

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ], 
+  imports: [ 
+    BrowserModule,
+    FormsModule 
+  ],
+  declarations: [ 
+    AppComponent,
+    ProductListComponent 
+  ], 
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
