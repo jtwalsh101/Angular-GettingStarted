bd1435d9ec39de68027d58ad148cec1254e25a00
jtwalsh101 <jtwalsh101@users.noreply.github.com>
2020-04-12 15:32:02 -0500

    Completed chapter 4

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -4,12 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [AppComponent]
+  imports: [ BrowserModule ],
+  declarations: [ AppComponent ], 
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
